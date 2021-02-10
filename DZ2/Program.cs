60e9e673282ee192cf8630d500da08e6676f8f83
i7allia <76435852+i7allia@users.noreply.github.com>
2021-02-10 15:02:59 +0300

    OpenText
    открываю файл через OpenText и построчно вывожу содержимое на экран через Console.WriteLine()

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
+using System.IO;
 namespace DZ2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!:/n");
+            using (StreamReader sr = File.OpenText("Staff.csv"))
+            {
+                string input = null;
+                while ((input = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(input);
+                }
+            }
+            Console.ReadLine();
         }
     }
-}
+            }    
+        
---
+++ DZ2/Staff.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ2
+{
+    class Staff
+    {
+    }
+}
